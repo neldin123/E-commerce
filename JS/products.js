2e135f64830765ea1ec833af139372b6b550a6c9
Nooreldin Abdelnaser <nbdalnasr52@gmail.com>
2026-04-28 22:04:00 +0300

    Update data fetching URL to use external API

--- JS/products.js
+++ JS/products.js
@@ -14,7 +14,7 @@ let allData = [];
 
 async function fetchingData() {
   try {
-    const response = await fetch("E-commerce/products.json");
+    const response = await fetch("https://api.npoint.io/0b0b852661cdfc864f7f");
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
